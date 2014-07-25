e837a4479d5a83b3b6d295bd9eb9c468b913a3bc
narkisr <narkisr@gmail.com>
2014-07-25 11:12:08 -0700

    Updating cmtool.sh variables to match .json templates

--- script/cmtool.sh
+++ script/cmtool.sh
@@ -1,68 +1,117 @@
 #!/bin/bash -eux
 
-# PROVISIONER and PROVISIONER_VERSION varaibles should be set inside of
-# Packer's template:
+# CM and CM_VERSION variables should be set inside of the Packer template:
 #
-# Valid values for PROVISIONER are:
-#   'provisionerless' -- build a box without a provisioner
-#   'chef'            -- build a box with the Chef provisioner
-#   'salt'            -- build a box with the Salt provisioner
+# Values for CM can be:
+#   'nocm'               -- build a box without a configuration management tool
+#   'chef'               -- build a box with Chef
+#   'chefdk'             -- build a box with Chef Development Kit
+#   'salt'               -- build a box with Salt
+#   'puppet'             -- build a box with Puppet
 #
-# When PROVISIONER != 'provisionerless' values for PROVISIONER_VERSION  are:
-#   'x.y.z'           -- build a box with version x.y.z of the Chef provisioner
-#   'x.y'             -- build a box with version x.y of the Salt provisioner
-#   'latest'          -- build a box with the latest version of the provisioner
+# Values for CM_VERSION can be (when CM is chef|chefdk|salt|puppet):
+#   'x.y.z'              -- build a box with version x.y.z of Chef
+#   'x.y'                -- build a box with version x.y of Salt
+#   'x.y.z-apuppetlabsb' -- build a box with package version of Puppet
+#   'latest'             -- build a box with the latest version
 #
-# Assume that PROVISIONER environment variable is set inside of Packer's
-# template.
-#
-# Set PROVISIONER_VERSION to 'latest' if unset because it can be problematic
+# Set CM_VERSION to 'latest' if unset because it can be problematic
 # to set variables in pairs with Packer (and Packer does not support
 # multi-value variables).
-PROVISIONER_VERSION=${PROVISIONER_VERSION:-latest}
+CM_VERSION=${CM_VERSION:-latest}
 
 #
-# Provisioner installs
+# Provisioner installs.
 #
 
 install_chef()
 {
-    echo "==> Installing Chef provisioner"
-    if [[ ${PROVISIONER_VERSION:-} == 'latest' ]]; then
-      echo "==> Installing latest Chef version"
-      curl -L https://www.opscode.com/chef/install.sh | sh
+    echo "==> Installing Chef"
+    if [[ ${CM_VERSION} == 'latest' ]]; then
+        echo "Installing latest Chef version"
+        curl -L https://www.getchef.com/chef/install.sh | bash
     else
-      echo "==> Installing Chef version ${PROVISIONER_VERSION}"
-      curl -L https://www.opscode.com/chef/install.sh | sh -s -- -v $PROVISIONER_VERSION
+        echo "Installing Chef version ${CM_VERSION}"
+        curl -L https://www.getchef.com/chef/install.sh | bash -s -- -v $CM_VERSION
     fi
-    if [[ ${PROVISIONER_SET_PATH:-} == 'true' ]]; then
-      echo "Automatically setting vagrant PATH to Chef Client"
-      echo 'export PATH="/opt/chef/embedded/bin:$PATH"' >> /home/vagrant/.bash_profile
+
+    if [[ ${CM_SET_PATH:-} == 'true' ]]; then
+        echo "Automatically setting vagrant PATH to Chef Client"
+        echo 'export PATH="/opt/chef/embedded/bin:$PATH"' >> /home/vagrant/.bash_profile
+        # Handy to have these packages install for native extension compiles
+        apt-get install -y libxslt-dev libxml2-dev
+    fi
+}
+
+install_chef_dk()
+{
+    echo "==> Installing Chef Development Kit"
+    if [[ ${CM_VERSION:-} == 'latest' ]]; then
+        echo "==> Installing latest Chef Development Kit version"
+        curl -L https://www.getchef.com/chef/install.sh | sh -s -- -P chefdk
+    else
+        echo "==> Installing Chef Development Kit ${CM_VERSION}"
+        curl -L https://www.getchef.com/chef/install.sh | sh -s -- -P chefdk -v ${CM_VERSION}
+    fi
+
+    if [[ ${CM_SET_PATH:-} == 'true' ]]; then
+        echo "Automatically setting vagrant PATH to Chef Development Kit"
+        echo 'export PATH="/opt/chefdk/embedded/bin:/home/vagrant/.chefdk/gem/ruby/2.1.0/bin:$PATH"' >> /home/vagrant/.bash_profile
     fi
 }
 
 install_salt()
 {
-    echo "==> Installing Salt provisioner"
-    if [[ ${PROVISIONER_VERSION:-} == 'latest' ]]; then
-      echo "==> Installing latest Salt version"
-      wget -O - http://bootstrap.saltstack.org | sudo sh
+    echo "==> Installing Salt"
+    if [[ ${CM_VERSION:-} == 'latest' ]]; then
+        echo "Installing latest Salt version"
+        wget -O - http://bootstrap.saltstack.org | sudo sh
     else
-      echo "Installing Salt version ${PROVISIONER_VERSION}"
-      curl -L http://bootstrap.saltstack.org | sudo sh -s -- git ${PROVISIONER_VERSION}
+        echo "Installing Salt version $CM_VERSION"
+        curl -L http://bootstrap.saltstack.org | sudo sh -s -- git $CM_VERSION
     fi
 }
 
-case "${PROVISIONER}" in
+install_puppet()
+{
+    echo "==> Installing Puppet"
+    . /etc/lsb-release
+
+    DEB_NAME=puppetlabs-release-${DISTRIB_CODENAME}.deb
+    wget http://apt.puppetlabs.com/${DEB_NAME}
+    dpkg -i ${DEB_NAME}
+    if [[ ${CM_VERSION:-} == 'latest' ]]; then
+      echo "Installing latest Puppet version"
+      apt-get install -y puppet
+    else
+      echo "Installing Puppet version $CM_VERSION"
+      apt-get install -y puppet-common=$CM_VERSION puppet=$CM_VERSION
+    fi
+    rm -f ${DEB_NAME}
+}
+
+#
+# Main script
+#
+
+case "${CM}" in
   'chef')
     install_chef
+    ;;
+
+  'chefdk')
+    install_chef_dk
     ;;
 
   'salt')
     install_salt
     ;;
 
+  'puppet')
+    install_puppet
+    ;;
+
   *)
-    echo "==> Building box without a provisioner"
+    echo "==> Building box without baking in a configuration management tool"
     ;;
 esac
