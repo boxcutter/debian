cceecbb60ad6290753b70472a727ab74d061298a
Stephen Reay <stephen.reay@me.com>
2017-03-28 14:10:41 +0700

    make sure udev overrides persist package updates

--- script/cleanup.sh
+++ script/cleanup.sh
@@ -4,7 +4,8 @@
 # http://6.ptmc.org/?p=164
 echo "cleaning up udev rules"
 rm -rf /dev/.udev/
-rm /lib/udev/rules.d/75-persistent-net-generator.rules
+# Better fix that persists package updates: http://serverfault.com/a/485689
+touch /etc/udev/rules.d/75-persistent-net-generator.rules
 
 echo "==> Cleaning up leftover dhcp leases"
 if [ -d "/var/lib/dhcp" ]; then
