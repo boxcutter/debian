ab6c384620b11c5d983539016a84afd0987f3d6c
Abhigyan Kumar Singh <singhabhigyankumar@gmail.com>
2020-10-18 15:32:28 +0530

    Changed the variable according to the issue-61

--- script/vagrant.sh
+++ script/vagrant.sh
@@ -2,21 +2,21 @@
 
 echo '==> Configuring settings for vagrant'
 
-SSH_USER=${SSH_USER:-vagrant}
+SSH_USERNAME=${SSH_USERNAME:-vagrant}
 SSH_USER_HOME=${SSH_USER_HOME:-/home/${SSH_USER}}
 VAGRANT_INSECURE_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"
 
 # Add vagrant user (if it doesn't already exist)
-if ! id -u $SSH_USER >/dev/null 2>&1; then
+if ! id -u $SSH_USERNAME >/dev/null 2>&1; then
     echo '==> Creating Vagrant user'
-    /usr/sbin/groupadd $SSH_USER
-    /usr/sbin/useradd $SSH_USER -g $SSH_USER -G sudo -d $SSH_USER_HOME --create-home
-    echo "${SSH_USER}:${SSH_USER}" | chpasswd
+    /usr/sbin/groupadd $SSH_USERNAME
+    /usr/sbin/useradd $SSH_USERNAME -g $SSH_USERNAME -G sudo -d $SSH_USER_HOME --create-home
+    echo "${SSH_USERNAME}:${SSH_USERNAME}" | chpasswd
 fi
 
 # Set up sudo.  Be careful to set permission BEFORE copying file to sudoers.d
 ( cat <<EOP
-%$SSH_USER ALL=(ALL) NOPASSWD:ALL
+%$SSH_USERNAME ALL=(ALL) NOPASSWD:ALL
 EOP
 ) > /tmp/vagrant
 chmod 0440 /tmp/vagrant
